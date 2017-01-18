9f592527556c23a7936fceaa173f5da28ea84f4f
Alastair Hughes <hobbitalastair@gmail.com>
2017-01-19 09:46:38 +1300

    Replaced db init code with the version in the backend repo

--- utility.go
+++ utility.go
@@ -132,66 +132,8 @@ func list(user string, db *sql.DB) {
 
 // initDB initialises the database with the expected tables
 func initDB(db *sql.DB) {
-	exec := []string{
-		`DROP TABLE views`,
-		`DROP TABLE owns`,
-		`DROP TABLE deliverables`,
-		`DROP TABLE projects`,
-		`DROP TABLE users`,
-		`CREATE TABLE users (
-			name VARCHAR(320) PRIMARY KEY, -- 320 is the maximum email length.
-			salt BYTEA,
-			password BYTEA, -- Password is salted and encrypted.
-			is_manager BOOL -- True if the user is also a manager (can create projects).
-		)`,
-		`CREATE TABLE projects (
-			id BIGINT PRIMARY KEY, -- Is this required??
-			name VARCHAR(128), -- Type??
-			percentage SMALLINT CHECK (percentage >= 0 and percentage <= 100),
-			description VARCHAR(512), -- Size??
-			flag BOOL,
-			flag_version INT
-		)`,
-		`CREATE TABLE deliverables (
-			id BIGINT,
-			pid BIGINT,
-			name VARCHAR(128),
-			due DATE,
-			percentage SMALLINT CHECK (percentage >= 0 and percentage <= 100),
-			description VARCHAR(512), -- Size??
-			PRIMARY KEY (id, pid)
-		)`,
-		`CREATE TABLE owns (
-			name VARCHAR(320) REFERENCES users,
-			pid BIGINT REFERENCES projects,
-			PRIMARY KEY (name, pid)
-		)`,
-		`CREATE TABLE views (
-			name VARCHAR(320) REFERENCES users,
-			pid BIGINT REFERENCES projects,
-			PRIMARY KEY (name, pid)
-		)`,
-		// Add a couple of test projects.
-		`INSERT INTO projects VALUES (0, 'Test Project 0', 30, 'First test project', TRUE, 0)`,
-		`INSERT INTO projects VALUES (1, 'Test Project 1', 80, 'Second test project', FALSE, 0)`,
-		`INSERT INTO deliverables VALUES
-			(0, 0, 'Deliverable 0', '11/25/2016', 20, 'Finish backend')`,
-		`INSERT INTO deliverables VALUES
-			(1, 0, 'Deliverable 1', '12/9/2016', 70, 'Finish prototype')`,
-		// Add a test user.
-		`INSERT INTO users VALUES ('test', '', '', TRUE)`,
-		`INSERT INTO owns VALUES ('test', 0)`,
-		`INSERT INTO views VALUES ('test', 1)`,
-	}
-
-	for _, cmd := range exec {
-		_, err := db.Exec(cmd)
-		if err != nil {
-			fmt.Printf("Error executing '%s': %q\n", cmd, err)
-		} else {
-			fmt.Printf("%s\n", cmd)
-		}
-	}
+	d := backend.NewDB(db)
+	d.Init()
 }
 
 // vim: sw=4 ts=4 noexpandtab
